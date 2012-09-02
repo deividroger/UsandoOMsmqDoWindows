691573b3e8636160cb962daaa757fa1e22eb4663
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2012-09-02 18:27:57 -0300

    Projeto console demonstrando com usar o msmq

---
+++ Console.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace Console.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = @".\private$\mensagem";
+
+            var mensagem = System.Console.ReadLine();
+
+            using (MessageQueue _fila = new MessageQueue(queue))
+            {
+                if (!MessageQueue.Exists(queue))
+                    MessageQueue.Create(queue);
+
+                _fila.Send(mensagem);
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
